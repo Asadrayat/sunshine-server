06b95b6a636d71ef25d08e29c10551d3a92633f7
Asadrayat <asad.rayat4@gmail.com>
2022-11-09 02:15:24 +0600

    Error indefiner for loader service

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
 const express = require('express');
 const cors = require('cors');
-
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 // middle wares 
 app.use(cors())
 app.use(express.json());
 
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.8nnhhq6.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        const serviceCollection = client.db('Sunshine').collection('services');
+        app.get('/services', async (req, res) => {
+            const query = {}
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        });
+    }
+    finally {
+
+    }
+}
 app.get('/', (req, res) => {
     res.send('Sunshine server is running');
 })
